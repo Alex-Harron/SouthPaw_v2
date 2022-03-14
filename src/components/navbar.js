277cfbccb73e2461899acb95d06e91408b435ba5
Alex-Harron <aharron.harron@gmail.com>
2022-03-14 17:35:17 -0400

    adding style

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container'
 
 function Navigation() {
     return (
-<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+<Navbar collapseOnSelect expand="lg" className='nav'>
   <Container>
   <Navbar.Brand href="#home">SouthPaw Marketplace</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -19,11 +19,10 @@ function Navigation() {
     <Nav>
       <Nav.Link href="#pricing">Cart</Nav.Link>
       <NavDropdown title="Account" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.1">Sign Out</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Your Cart</NavDropdown.Item>
         <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">View Profile</NavDropdown.Item>
       </NavDropdown>
     </Nav>
   </Navbar.Collapse>
